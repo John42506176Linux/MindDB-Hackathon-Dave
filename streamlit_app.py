336ca71503242a8dd1eaaed52f3eda3487cb5198
John42506176Linux <johnkongtcheu@gmail.com>
2024-06-01 14:22:53 -0700

    Commiting local code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from streamlit_chat import message
+from streamlit_mic_recorder import mic_recorder
 
 st.title("Dave- An AI Software Architect in your pocket")
 uploaded_file = st.sidebar.file_uploader("Upload your Requirements Files", type="txt")
@@ -9,7 +10,10 @@ if not uploaded_file:
     st.error("Please upload brainstorming/chat transcript to continue!")
     st.stop()
 else:
+    st.success("File uploaded successfully!")
     print("File uploaded")
+    with open('output.txt', 'w') as f:
+        f.write(uploaded_file.getvalue())
     st.button('Start')
 
 def main():
@@ -30,5 +34,55 @@ def main():
         # Add user message to chat history
         st.session_state.messages.append({"role": "user", "content": prompt})
 
+
+def whisper_stt(openai_api_key=None, start_prompt="Start recording", stop_prompt="Stop recording", just_once=False,
+               use_container_width=False, language=None, callback=None, args=(), kwargs=None, key=None):
+    if not 'openai_client' in st.session_state:
+        st.session_state.openai_client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+    if not '_last_speech_to_text_transcript_id' in st.session_state:
+        st.session_state._last_speech_to_text_transcript_id = 0
+    if not '_last_speech_to_text_transcript' in st.session_state:
+        st.session_state._last_speech_to_text_transcript = None
+    if key and not key + '_output' in st.session_state:
+        st.session_state[key + '_output'] = None
+    audio = mic_recorder(start_prompt=start_prompt, stop_prompt=stop_prompt, just_once=just_once,
+                         use_container_width=use_container_width, key=key)
+    new_output = False
+    if audio is None:
+        output = None
+    else:
+        id = audio['id']
+        new_output = (id > st.session_state._last_speech_to_text_transcript_id)
+        if new_output:
+            output = None
+            st.session_state._last_speech_to_text_transcript_id = id
+            audio_bio = io.BytesIO(audio['bytes'])
+            audio_bio.name = 'audio.mp3'
+            success = False
+            err = 0
+            while not success and err < 3:  # Retry up to 3 times in case of OpenAI server error.
+                try:
+                    transcript = st.session_state.openai_client.audio.transcriptions.create(
+                        model="whisper-1",
+                        file=audio_bio,
+                        language=language
+                    )
+                except Exception as e:
+                    print(str(e))  # log the exception in the terminal
+                    err += 1
+                else:
+                    success = True
+                    output = transcript.text
+                    st.session_state._last_speech_to_text_transcript = output
+        elif not just_once:
+            output = st.session_state._last_speech_to_text_transcript
+        else:
+            output = None
+
+    if key:
+        st.session_state[key + '_output'] = output
+    if new_output and callback:
+        callback(*args, **(kwargs or {}))
+    return output
 if __name__ == "__main__":
     main()
